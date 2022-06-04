d547137138f175cca49220e374a54155f4be0f31
GuzRam <lalubagoes2@gmail.com>
2022-06-04 22:15:49 +0800

    Tailwind-CSS

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors : {
+        primary: '#505ab8',
+        secondary: '#475569',
+        yellow: '#e17129',
+      },
+    screens: {
+      '2xl': '1280px',
+    }
+    },
+  },
+  plugins: [],
+};
